0fde421c57349498254eb7e67ec95d90729079b9
Karan Dalakoti <140832730+Dalakoti01@users.noreply.github.com>
2024-04-08 11:40:36 +0000

    more about functions

--- functions.js
+++ functions.js
@@ -57,3 +57,44 @@ console.log(loginUserName("karan" ));
 
 console.log(loginUserName());
 
+// More about Functions
+
+function addToMyCart(...num1){ //  These three dots are calledas rest operator and can store any number of avlues into an array 
+    return num1;
+}
+console.log(addToMyCart(234,432,1211,123,43));
+
+// The above functions which is using rest operator is very much useful when we are using an e-commerce website as at the time of 
+// functions creation we are unaware about how many elements are coming in the cart .  So by using rest operator we can store 
+// any number of items into the cart using an array and afterwards using for loop we can easily find out its sum
+
+function addToMyCart1(val1, val2, ...num1){ //  These three dots are calledas rest operator and can store any number of avlues into an array 
+    return num1;
+}
+console.log(addToMyCart1(234,432,1211,123,43)); //Here the first two values will be given to val1 and val 2 respectively and all the
+// other values will be stored into an array using an rest operator
+
+// How to pass an object inside an function
+
+const user = {
+    userName : "Karan",
+    salary : 10000000
+
+}
+
+function handleObject (anyObject){
+    console.log(`Username is ${user.userName} and his salary is ${user.salary}` );
+}
+
+console.log(handleObject(user));
+
+// How to pass an array using an function
+
+const myNewArray = [200,400,100,160]
+function returnMySecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnMySecondValue(myNewArray));
+// console.log(returnMySecondValue([200,400,100,160])); // both will give the same output
+                                                
